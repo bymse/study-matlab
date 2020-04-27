13a2a9c20e61741e2604db9025dc685cdb46ef65
bymse <egorstrelov@gmail.com>
2020-04-28 00:44:12 +0300

    hw/10.04 Задание 4

---
+++ task/task_4.m
@@ -0,0 +1,24 @@
+n = input('n=');
+syms x
+my_poly_symb = x^(2*n) - n*x^(n+1)+n*x^(n-1) - 1;
+my_poly = coeffs(my_poly_symb, 'all');
+disp('Poly');
+disp(my_poly);
+
+my_roots = roots(my_poly);
+disp('Roots');
+disp(my_roots);
+
+idx = (my_roots==real(my_roots));
+real_roots = my_roots(idx);
+
+max_val = max(real_roots);
+min_val = min(real_roots);
+
+if max_val == min_val
+    disp('Segment has zero lenght');
+end
+
+x = linspace(min_val, max_val);
+poly_vals = polyval(double(my_poly), double(x));
+plot(x, poly_vals ); 
